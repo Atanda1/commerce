4e7a061cf76c285af4746217a36b0268cc6b8b66
Atanda1 <45256233+Atanda1@users.noreply.github.com>
2019-11-27 15:40:39 +0100

    Updates

---
+++ precache-manifest.04e1cb5260ccbc20b27a97b23704cc04.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9525d09541496db6b4e632f27402f253",
+    "url": "/commerce/index.html"
+  },
+  {
+    "revision": "7888fbb96efbf348b126",
+    "url": "/commerce/static/css/main.9e174790.chunk.css"
+  },
+  {
+    "revision": "628b43e890e2b5cc42ad",
+    "url": "/commerce/static/js/2.056730a5.chunk.js"
+  },
+  {
+    "revision": "7888fbb96efbf348b126",
+    "url": "/commerce/static/js/main.132332b3.chunk.js"
+  },
+  {
+    "revision": "20ee5a96dcb65e6f0ca2",
+    "url": "/commerce/static/js/runtime~main.d51b6905.js"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "/commerce/static/media/crown.ed28accb.svg"
+  }
+]);
